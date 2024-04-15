8628ebaa1c6e1189b59d889c1f7fa80f981d3592
robin.sauma@tietoevry.com <robin.sauma@tietoevry.com>
2024-04-15 12:31:08 +0200

    moved button down

--- src/Pages/BettingPage/Betting.tsx
+++ src/Pages/BettingPage/Betting.tsx
@@ -122,11 +122,11 @@ const BettingPage = () => {
 
         setSending(false);
         setStatus("You successfully created the betslip")
-        
+
         return (<Popup />);
       });
 
-      
+
       setSending(false);
       setStatus("Code not found")
     } catch (error) {
@@ -147,87 +147,85 @@ const BettingPage = () => {
 
   return (
     <>
-    
-    <div className="flex justify-center items-center">
-    <img src="/images/logo.png" alt="logo" className="cursor-pointer h-65  w-60 md:w-10 lg:w-48"  />
-    </div>
-    <PoolInfo />
-    
-    <div className="flex flex-wrap gap-4 justify-center pt-5">
-          <div className="flex flex-col gap-2">
+
+      <div className="flex justify-center items-center">
+        <img src="/images/logo.png" alt="logo" className="cursor-pointer h-65  w-60 md:w-10 lg:w-48" />
+      </div>
+      <PoolInfo />
+
+      <div className="flex flex-wrap gap-4 justify-center pt-5">
+        <div className="flex flex-col gap-2">
           <input type="text" className='X-form-control' placeholder='code' style={{ maxWidth: '300px' }} onChange={(e) => setCode(e.target.value)} />
-          </div>
-          <div className="flex flex-col gap-2">
-          
-          <input type="text" className='X-form-control' placeholder='wallet' style={{ maxWidth: '300px' }} onChange={(e) => setWallet(e.target.value)} />
-          </div>
-          
-        {
-          isSendable && (
-            <div>
-            <button className="bg-green-500 hover:bg-green-700 active:bg-green-800 px-4 py-2 rounded-md text-white" onClick={send}> Send </button>
-          </div>
-          )
-        }
-
-        {
-          !isSendable && (
-            <div>
-            <button className="bg-[#e4e4e4] py-2 px-7 mx-4 rounded-full cursor-pointer"> Create My PIX Slip</button>
-            </div>
-          )
-        }
         </div>
-    <div className='centeralized-container'>
-      
-      <table className="custom-table">
-        
-      
-        <tbody>
-          {games.map((game, index) => (
-            <tr key={index} className='gameBorder'>
-              <td style={{ width: '20%', paddingRight: 0 }}>
-                <p style={{ color: 'gray' }}>{game.league}</p>
-                <img src="/" alt="" />
-                <p>{game.homeTeam}</p>
-              </td>
-              <td valign='middle'>
-                <p>vs</p>
-              </td>
-              <td style={{ textAlign: 'right', paddingLeft: 0, width: '20%' }}>
-                <p style={{ color: 'gray' }}>{game.date}</p>
-                <img src="/" alt="" />
-                <p>{game.awayTeam}</p>
-              </td>
-              <td valign='middle'>
-                <div className='checkboxContainer'>
-                  {renderCheckboxes(game, index)}
+        <div className="flex flex-col gap-2">
 
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+          <input type="text" className='X-form-control' placeholder='wallet' style={{ maxWidth: '300px' }} onChange={(e) => setWallet(e.target.value)} />
+        </div>
 
+      </div>
+      <div className='centeralized-container'>
+
+        <table className="custom-table">
+
+
+          <tbody>
+            {games.map((game, index) => (
+              <tr key={index} className='gameBorder'>
+                <td style={{ width: '20%', paddingRight: 0 }}>
+                  <p style={{ color: 'gray' }}>{game.league}</p>
+                  <img src="/" alt="" />
+                  <p>{game.homeTeam}</p>
+                </td>
+                <td valign='middle'>
+                  <p>vs</p>
+                </td>
+                <td style={{ textAlign: 'right', paddingLeft: 0, width: '20%' }}>
+                  <p style={{ color: 'gray' }}>{game.date}</p>
+                  <img src="/" alt="" />
+                  <p>{game.awayTeam}</p>
+                </td>
+                <td valign='middle'>
+                  <div className='checkboxContainer'>
+                    {renderCheckboxes(game, index)}
+
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div>
+          {
+            isSendable && (
+              <button className="bg-green-500 hover:bg-green-700 active:bg-green-800 px-4 py-2 rounded-md text-white" onClick={send}> Send </button>
+            )
+          }
+
+          {
+            !isSendable && (
+              <button className="bg-[#e4e4e4] py-2 px-7 mx-4 rounded-full cursor-pointer"> Create My PIX Slip</button>
+            )
+          }
+        </div>
       
       <div className='custom-table' style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <p>{status}</p>
-        {sending && (
-          <div role="status">
-            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
+          <p>{status}</p>
+          {sending && (
+            <div role="status">
+              <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
 
-        )}
+          )}
+        </div>
+
+
+
       </div>
-
-
-
-    </div>
     </>
   );
 };
