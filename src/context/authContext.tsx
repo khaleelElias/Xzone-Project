04025961a8137f08a99fcdbc93e59cc7093445d3
robin.sauma@tietoevry.com <robin.sauma@tietoevry.com>
2024-05-18 00:48:22 +0200

    wallet

---
+++ src/context/authContext.tsx
@@ -0,0 +1,105 @@
+import { ReactNode, createContext, useState } from "react";
+import { useQuery } from "react-query";
+import { POST } from "../helper/apiHelper";
+import {
+    Button,
+    Dialog,
+    DialogHeader,
+    DialogBody,
+    DialogFooter,
+  } from "@material-tailwind/react";
+import Modal from "../components/Modal/Modal";
+
+type Props = {
+    children?: ReactNode;
+}
+
+type IAuthContext = {
+    authenticated: boolean;
+    authenticate: () => void;
+}
+
+const initialValue: IAuthContext = {
+    authenticated: false,
+    authenticate: async () => { }
+}
+
+const AuthContext = createContext<IAuthContext>(initialValue);
+
+const AuthProvider = ({ children }: Props) => {
+    const [authenticated, setAuthenticated] = useState<boolean>(initialValue.authenticated);
+    const [registerOpen, setRegisterOpen] = useState<boolean>(false);
+    const [walletAddress, setWalletAddress] = useState("");
+    const getProvider = () => {
+        if ('phantom' in window) {
+            const phantom = window.phantom as any;
+
+            const provider = phantom?.solana;
+            if (provider?.isPhantom) {
+                return provider;
+            }
+        }
+
+        window.open('https://phantom.app/', '_blank');
+    };
+
+    const authenticate = async () => {
+        let provider = getProvider();
+        if (!provider)
+            return;
+
+        try {
+            const resp = await provider.connect({ onlyIfTrusted: true });
+            setWalletAddress(resp.publicKey.toString());
+
+            const response = await POST("/auth/user/login", {
+                signature: resp.publicKey.toString(),
+                walletAddress: resp.publicKey.toString()
+            });
+
+            if(response.successful) {
+
+            } else {
+                if(response.status == 404) {
+                    setRegisterOpen(true);
+                } else {
+                    //TODO: warn user?
+                    console.log(response.message);
+                }
+            }
+
+            // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo 
+
+        } catch (err) {
+            // { code: 4001, message: 'User rejected the request.' }
+        }
+    }
+
+    const register = async () => {
+        
+        const response = await POST("/auth/user/register", {
+            walletAddress: walletAddress
+        });
+
+        if(!response.successful) {
+            //TODO: warn user?
+            console.log("error")
+        } else {
+            setRegisterOpen(false);
+            authenticate();
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ authenticated, authenticate }}>
+            <Modal open={false} title={"Terms of servie"}>
+                <p>brororororooooooor du måste acceptera detta</p>
+                <button onClick={register}>I Accept</button>
+                <button onClick={() => setRegisterOpen(false) }>I Decline</button>
+            </Modal>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export { AuthContext, AuthProvider }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Navbar />
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
